b8923e55b167840fa786e56c0c13ce0da6eb29fe
Beny.K <Beku1997@gmail.com>
2021-11-13 23:22:45 +0200

    fix

--- js/pages/app-home.cmp.js
+++ js/pages/app-home.cmp.js
@@ -4,13 +4,9 @@ export default {
                
             <div class="home-page-hero">
             
-<<<<<<< HEAD
- <h3>Welcome To  </h3><span class="the-home-logo"> Appsus <img class="icon-home-page" src="./../img/icon.png" /></span>
-              <h2>Everything in one place</h2>
-=======
+ 
  <h3 class="home-page-txt">Welcome To  </h3><span><img class="icon-home-page" src="./img/logo.png" /></span>
               <h2 class="home-page-txt">Everything in one place</h2>
->>>>>>> 430d2c5be0c70c4a57a68bd53762eb5c54f2e32a
               
             </div>
         </section>
