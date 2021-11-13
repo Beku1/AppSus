514097e25009542a165e4955b6937b9f6a998197
Daniel Radia <danielradia21@gmail.com>
2021-11-13 23:43:55 +0200

    finito la comedia

--- js/pages/app-about.cmp.js
+++ js/pages/app-about.cmp.js
@@ -5,7 +5,7 @@ export default {
         <h1 class="about-header-forget">O M G - We forgot to create an About Page...</h1>
         <p class="about-page-content">
        <h2 style="text-decoration:underline"> We are joking of course! </h2>
-       This page is currently under <span class="bold">renovation</span>, but the  <span class="bold">Mail </span>system and the <span class="bold">Notes</span> system are still working!
+       This page is currently under <span class="bold">renovation</span>, but the  <span class="bold">Mail</span> system and the <span class="bold">Notes</span> system are still working!
         You can click on one of the icons at the <span class="bold">top</span> of the site to enjoy the site services!
         </p>
         </div>
