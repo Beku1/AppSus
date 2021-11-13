1aa9c0696eb67bbd4538909c8b1413afc5ec2f05
Beny.K <Beku1997@gmail.com>
2021-11-13 22:27:57 +0200

    txt and other

--- js/pages/app-home.cmp.js
+++ js/pages/app-home.cmp.js
@@ -4,8 +4,8 @@ export default {
                
             <div class="home-page-hero">
             
- <h3>Welcome To  </h3><span><img class="icon-home-page" src="./img/logo.png" /></span>
-              <h2>Everything in one place</h2>
+ <h3 class="home-page-txt">Welcome To  </h3><span><img class="icon-home-page" src="./img/logo.png" /></span>
+              <h2 class="home-page-txt">Everything in one place</h2>
               
             </div>
         </section>
